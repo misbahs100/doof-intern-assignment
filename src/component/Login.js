93e50011a37f640e97aab7b75ab91a75d4957058
misbahs100 <misbahuddins100@gmail.com>
2021-06-06 19:15:10 +0600

    some css

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,56 +1,59 @@
 import React from 'react';
 import './Login.css';
 import login from '../images/Login.png';
-import { Box} from '@material-ui/core';
-// import { spacing } from '@material-ui/system';
+import { Box, Grid } from '@material-ui/core';
 
 const Login = () => {
     return (
-        <div className="background" style={{ marginTop: '10%' }}>
-            <Box my="auto" className="login container" style={{ border: '1px solid red' }}>
-                <div className="row container" >
-                    <div className="col-md-6" style={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
+        <Box style={{ padding: "10px", border: '1px solid red' }}>
+            <Box p={6}>
+                <Box container my="auto" className="login " style={{ border: '1px solid red' }}>
+                    <Box px={5}>
+                        <Grid container direction="row" className="">
+                            <Grid item xs={12} sm={12} md={6} className="" style={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
 
-                        <Box display="flex" justifyContent="flex-start" className="test"><h1 className="doof">DOOF</h1></Box>
-                        <Box mt={1}>
-                            <h4>Login</h4>
-                           <Box mt={4}>
-                           <form className="" action="">
-                                <label className="input-text" htmlFor="">Email</label><br />
-                                <input type="email" name="" placeholder="Email" />
-                                <br /><br />
-                                <label className="input-text" htmlFor="Password">Password</label><br />
-                                <input type="password" name="" placeholder="Password" /> <small className="forgot">Forgot Password</small>
-                                <br />
-                                <br />
-                                <button className="btn btn-warning w-50 login-text" type="submit">Login</button>
-                            </form>
-                           </Box>
-                           <br />
-                            <p className="account">Don’t have an account yet? <b>Signup</b></p>
+                                <Box display="flex" justifyContent="flex-start" className="test"><h1 className="doof">DOOF</h1></Box>
+                                <Box mt={1}>
+                                    <h4>Login</h4>
+                                    <Box mt={4}>
+                                        <form className="" action="">
+                                            <label className="input-text" htmlFor="">Email</label><br />
+                                            <input type="email" name="" placeholder="Email" />
+                                            <br /><br />
+                                            <label className="input-text" htmlFor="Password">Password</label><br />
+                                            <input type="password" name="" placeholder="Password" /> <small className="forgot">Forgot Password</small>
+                                            <br />
+                                            <br />
+                                            <button className="w-50 login-text" type="submit">Login</button>
+                                        </form>
+                                    </Box>
+                                    <br />
+                                    <p className="account">Don’t have an account yet? <b>Signup</b></p>
 
-                        </Box>
+                                </Box>
 
-                        <div className="terms" style={{ marginTop: '30%', display: 'flex' }}>
-                            <Box ><small className="">Terms of Service</small></Box>
-                            <Box mx={4}><small className="">Privacy Policy</small></Box>
-                        </div>
+                                <Box mt={20} className="terms" style={{ display: 'flex' }}>
+                                    <Box ><small className="">Terms of Service</small></Box>
+                                    <Box mx={4}><small className="">Privacy Policy</small></Box>
+                                </Box>
 
-                    </div>
- 
+                            </Grid>
 
-                    <div className="col-md-6" style={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
 
-                        <Box display="flex" justifyContent="flex-end"><button className="btn test test-btn d-flex justify-content-center">Test</button></Box>
-                        <Box mt={5}><h4 className="unlock">Unlock your stuck revenue</h4></Box>
-                        <p className="help">We help you unlock the true potential connect with your customers to <br /> understand the rights and wrongs with your product.</p>
-                        <Box mt={4} ml="auto"><img className="" style={{ width: '300px' }} src={login} alt="" /></Box>
+                            <Grid item xs={12} sm={12} md={6} style={{ display: 'flex', flexDirection: 'column', position: 'relative' }}>
 
-                    </div>
-                </div>
+                                <Box display="flex" justifyContent="flex-end"><button className="test test-btn">Test</button></Box>
+                                <Box mt={5}><h4 className="unlock">Unlock your stuck revenue</h4></Box>
+                                <p className="help">We help you unlock the true potential connect with your customers to <br /> understand the rights and wrongs with your product.</p>
+                                <Box mt={4} ml="auto"><img className="" style={{ width: '300px' }} src={login} alt="" /></Box>
 
+                            </Grid>
+                        </Grid>
+                    </Box>
+
+                </Box>
             </Box>
-        </div>
+        </Box>
     );
 };
 
